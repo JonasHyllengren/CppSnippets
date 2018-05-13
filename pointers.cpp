669934cf30bacfb5699b3ad20d8759712f4334be
Jonas Hyllengren <jonas.hyllengren@gmail.com>
2018-05-13 17:01:20 +0200

    Adds functions to the 'pointers' example

--- pointers.cpp
+++ pointers.cpp
@@ -2,6 +2,14 @@
 
 using namespace std;
 
+void printI(int i){
+   cout << i << endl;
+}
+
+void printP(int* i){
+   cout << i << endl;
+}
+
 int main(){
 
    int a{2};         // a is an integer
@@ -12,5 +20,10 @@ int main(){
    cout << p << endl;   // print adress of a again
    cout << *p << endl;  // print a again
 
-
+   // Do the same thing as above with functions
+   cout << "Now with functions!" << endl;  
+   printI(a);
+   printP(&a);
+   printP(p);
+   printI(*p);
 }
