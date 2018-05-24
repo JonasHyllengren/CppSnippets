1f45a3864c81d01eee1e14da15f4d5c838391151
Jonas Hyllengren <jonas.hyllengren@gmail.com>
2018-05-24 21:31:47 +0200

    Fix dice lab to be runable

--- course/dice/dice.h
+++ course/dice/dice.h
@@ -1,10 +1,8 @@
 #pragma once
 
-#include "stdafx.h"
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-
 
 class seedClass {
 public:
--- course/dice/main.cpp
+++ course/dice/main.cpp
@@ -1,7 +1,5 @@
-
 #include <iostream>
 #include "dice.h"
-
 
 /******************* TEST CODE ************************/
 
